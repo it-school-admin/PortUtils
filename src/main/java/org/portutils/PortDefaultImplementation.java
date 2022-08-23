53c3e273b1f8f2215882f39b34c40d348824dc00
it-school-admin <Dm0HY4sUiH>
2022-08-23 11:01:04 +0400

    Some refactorings

--- src/main/java/org/portutils/PortDefaultImplementation.java
+++ src/main/java/org/portutils/PortDefaultImplementation.java
@@ -16,24 +16,21 @@ class PortDefaultImplementation implements Port {
     private final SortedSet<List<Integer>> generatedIndexes;
 
     PortDefaultImplementation(String[] indexes) {
-        if (indexes == null)
-        {
+        if (indexes == null) {
             throw new IllegalArgumentException(PORT_DESCRIPTION_SHOULDNT_BE_NULL);
         }
 
-        if (indexes.length == 0)
-        {
+        if (indexes.length == 0) {
             throw new IllegalArgumentException(PORT_DESCRIPTION_SHOULDNT_BE_EMPTY);
         }
         portIndexParts = createUnmodifiableCollection(generateNumberSequences(indexes));
-        generatedIndexes = new IndexGenerator(portIndexParts).generate();
+        generatedIndexes = generateIndexes(portIndexParts);
     }
 
     private List<SortedSet<Integer>> createUnmodifiableCollection(List<SortedSet<Integer>> portIndexPartsModifiable) {
         final List<SortedSet<Integer>> portIndexParts;
         List<SortedSet<Integer>> unmodifiableLists = new ArrayList<>();
-        for (SortedSet<Integer> numbers: portIndexPartsModifiable)
-        {
+        for (SortedSet<Integer> numbers : portIndexPartsModifiable) {
             unmodifiableLists.add(unmodifiableSortedSet(numbers));
         }
 
@@ -55,87 +52,66 @@ class PortDefaultImplementation implements Port {
         return generatedIndexes;
     }
 
+    //Due to big complexity of algorithm think about to make implementation with parallel processing
+    public SortedSet<List<Integer>> generateIndexes(List<SortedSet<Integer>> portIndexParts) {
+        IndexMultiplicator indexMultiplicator = new IndexMultiplicator();
+        portIndexParts.forEach(indexMultiplicator::addNumbers);
+        return indexMultiplicator.getResult();
+    }
 
-    static class IndexGenerator {
-        private final List<SortedSet<Integer>> portSequences;
+    private class IndexMultiplicator {
+        private List<List<Integer>> indexes = new ArrayList<>();
 
-        public IndexGenerator(List<SortedSet<Integer>> portIndexes) {
-            this.portSequences = portIndexes;
+        public void addNumbers(SortedSet<Integer> sequenceOfNumbers) {
+            List<List<Integer>> prefixes = indexes;
+
+            if (prefixes.size() == 0) {
+                for (Integer number : sequenceOfNumbers) {
+                    prefixes.add(singletonList(number));
+                }
+            } else {
+                List<List<Integer>> newIndexes = new ArrayList<>();
+                for (List<Integer> index : prefixes) {
+                    for (Integer number : sequenceOfNumbers) {
+                        List<Integer> newIndex = new ArrayList<>(index);
+                        newIndex.add(number);
+                        newIndexes.add(newIndex);
+                    }
+                }
+                indexes = newIndexes;
+            }
+
         }
 
-        public SortedSet<List<Integer>> generate() {
-            IndexMultiplicator indexMultiplicator = new IndexMultiplicator();
-            for (SortedSet<Integer> sequenceOfNumbers: portSequences)
-            {
-                indexMultiplicator.addNumbers(sequenceOfNumbers);
+        public SortedSet<List<Integer>> getResult() {
+            List<List<Integer>> listWithUnmodifiableElements = new ArrayList<>();
+            for (List<Integer> index : indexes) {
+                listWithUnmodifiableElements.add(unmodifiableList(index));
             }
 
-            return indexMultiplicator.getResult();
-        }
+            //Tree set is used in order to give a interface with strict contract. In order to
+            // increase performance it is possible not to use TreeSet, just to use list.
+            //Using algorithm gives an ability to get sorted results wit simple List.
+            //But it seems that decreasing of performance with using TreeSet don't affect performance dramatically.
 
-        private class IndexMultiplicator {
-            private List<List<Integer>> indexes = new ArrayList<>();
-
-            public void addNumbers(SortedSet<Integer> sequenceOfNumbers) {
-                List<List<Integer>> prefixes = indexes;
-
-                if (prefixes.size() == 0)
-                {
-                    for (Integer number: sequenceOfNumbers) {
-                        prefixes.add(singletonList(number));
+            TreeSet<List<Integer>> result = new TreeSet<>((o1, o2) -> {
+                for (int i = 0; i < o1.size(); i++) {
+                    Integer firstNumber = o1.get(i);
+                    Integer secondNumber = o2.get(i);
+                    if (firstNumber > secondNumber) {
+                        return 1;
+                    } else if (firstNumber < secondNumber) {
+                        return -1;
                     }
                 }
-                else
-                {
-                    List<List<Integer>> newIndexes = new ArrayList<>();
-                    for (List<Integer> index: prefixes)
-                    {
-                        for (Integer number: sequenceOfNumbers)
-                        {
-                            List<Integer> newIndex = new ArrayList<>(index);
-                            newIndex.add(number);
-                            newIndexes.add(newIndex);
-                        }
-                    }
-                    indexes = newIndexes;
-                }
-
-            }
-
-            public SortedSet<List<Integer>> getResult() {
-                List<List<Integer>> listWithUnmodifiableElements = new ArrayList<>();
-                for (List<Integer> index: indexes)
-                {
-                    listWithUnmodifiableElements.add(unmodifiableList(index));
-                }
-
-                //Tree set is used in order to give a interface with strict contract. In order to
-                // increase performance it is possible not to use TreeSet, just to use list.
-                //Using algorithm gives an ability to get sorted results wit simple List.
-                //But it seems that decreasing of performance with using TreeSet don't affect performance dramatically.
-
-                TreeSet<List<Integer>> result = new TreeSet<>((o1, o2) -> {
-                    for(int i = 0; i<o1.size(); i++)
-                    {
-                        Integer firstNumber = o1.get(i);
-                        Integer secondNumber = o2.get(i);
-                        if (firstNumber>secondNumber)
-                        {
-                            return 1;
-                        }
-                        else if (firstNumber<secondNumber)
-                        {
-                            return -1;
-                        }
-                    }
-                    return 0;
-                });
+                return 0;
+            });
 
 
-                result.addAll(listWithUnmodifiableElements);
+            result.addAll(listWithUnmodifiableElements);
 
-                return unmodifiableSortedSet(result);
-            }
+            return unmodifiableSortedSet(result);
         }
     }
+
 }
