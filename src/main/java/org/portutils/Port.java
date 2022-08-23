f08f3aa722053dd48525b978b3d230a56f613edd
it-school-admin <Dm0HY4sUiH>
2022-08-23 09:34:30 +0400

    Making PortDescriptionElementParser package local. Adding java doc and some small changes.

--- src/main/java/org/portutils/Port.java
+++ src/main/java/org/portutils/Port.java
@@ -3,13 +3,20 @@ package org.portutils;
 import java.util.List;
 import java.util.SortedSet;
 
+/**
+ * Main interface that implements port functionality that can be created with @{@link PortDescriptionParser}
+ */
 public interface Port {
 
-    //TODO javadoc
     /**
-     *
-     * @return unmodifiable sorted
+     * @return unmodifiable sorted collection of indexes that is combination of given description.
+     * Each element of resulting set is unmodifiable list.
      */
     SortedSet<List<Integer>> getIndexes();
+
+    /**
+     * @return unmodifiable list of sorted numbers that the result of the parsing given descriptions. Each element
+     * of the list is the unmodifiable sorted set.
+     */
     List<SortedSet<Integer>> getNumbers();
 }
--- src/main/java/org/portutils/PortDescriptionParser.java
+++ src/main/java/org/portutils/PortDescriptionParser.java
@@ -1,18 +1,22 @@
 package org.portutils;
 
+/**
+ * The class contains static method that gives an ability to create @{@link Port} object.
+ */
 public class PortDescriptionParser {
 
-    //TODO javadoc
-
     /**
+     * The method returns Port element that is build with given array of indexes.
+     * @param indexes array of strings that describes the port indexes. Each element can contains a single value,
+     *                comma-separated list or interval description with dash-separator.
+     *                For example "90-95, 1, 4, 3,2"
      *
-     * @param numbersAndIntervals
-     * @return
+     * @return - created with given indexes @{@link Port} object
      */
 
-    public static Port parsePort(/*@NotNull*/ String[] numbersAndIntervals)
+    public static Port parsePort(/*@NotNull*/ String[] indexes)
     {
-        return new PortDefaultImplementation(numbersAndIntervals);
+        return new PortDefaultImplementation(indexes);
     }
 
 }
